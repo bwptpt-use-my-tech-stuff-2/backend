0713de77865c188270ea619b4f9017b9404158b3
Paul M Edwards <pauledwards@gmail.com>
2020-02-26 22:46:28 -0800

    Add knex migration file.

--- data/migrations/20200226204745_umts.js
+++ data/migrations/20200226204745_umts.js
@@ -1,28 +1,178 @@
 exports.up = function(knex) {
   return knex.schema
-    .createTable('Users', tbl => {})
-    .createTable('Stuff', tbl => {})
-    .createTable('Categories', tbl => {})
-    .createTable('Conditions', tbl => {})
-    .createTable('User_Stuff', tbl => {})
-    .createTable('User_FavoriteStuff', tbl => {})
-    .createTable('Rentals', tbl => {})
-    .createTable('Rental_Stuff', tbl => {})
-    .createTable('User_Rental_Owners', tbl => {})
-    .createTable('User_Rental_Renters', tbl => {})
-    .createTable('Reviews', tbl => {})
+    .createTable('Users', tbl => {
+      tbl.increments();
+      tbl.text('Email')
+        .unique()
+        .notNullable();
+      tbl.text('Password')
+        .notNullable();
+      tbl.text('FirstName')
+        .notNullable();
+      tbl.text('LastName')
+        .notNullable();
+      tbl.text('Location')
+        .notNullable();
+      tbl.text('ImagePath');
+      tbl.text('Phone');
+    })
+    .createTable('Categories', tbl => {
+      tbl.increments();
+      tbl.text('Category')
+        .unique()
+        .notNullable();
+    })
+    .createTable('Conditions', tbl => {
+      tbl.increments();
+      tbl.text('Condition')
+        .unique()
+        .notNullable();
+    })
+    .createTable('Stuff', tbl => {
+      tbl.increments();
+      tbl.integer('category_id')
+        .notNullable()
+        .unsigned()
+        .references('Categories.id');
+      tbl.text('Title')
+        .notNullable();
+      tbl.text('Description');
+      tbl.integer('condition_id')
+        .notNullable()
+        .unsigned()
+        .references('Conditions.id');
+      tbl.datetime('AddDate');
+      tbl.decimal('PricePerHour')
+        .notNullable();
+      tbl.decimal('PricePerDay')
+        .notNullable();
+      tbl.boolean('Available')
+        .notNullable()
+        .defaultTo(1);
+      tbl.text('ImagePath');
+    })
+    .createTable('Rentals', tbl => {
+      tbl.increments();
+      tbl.text('Title')
+        .notNullable();
+      tbl.text('Description');
+      tbl.text('PickupLocation');
+      tbl.datetime('PickupDateTime');
+      tbl.text('ReturnLocation');
+      tbl.datetime('ReturnDateTime');
+      tbl.integer('Duration')
+        .unsigned();
+      tbl.boolean('Term')
+        .unsigned();
+      tbl.boolean('Paid')
+        .notNullable()
+        .defaultTo(0);
+      tbl.boolean('Returned')
+        .notNullable()
+        .defaultTo(0);
+      tbl.decimal('RentalTotalCost');
+    })
+    .createTable('Reviews', tbl => {
+      tbl.increments();
+      tbl.integer('rental_id')
+        .notNullable()
+        .unsigned()
+        .references('Rentals.id');
+      tbl.integer('OwnerRatingOfRenter')
+        .unsigned();
+      tbl.integer('RenterRatingOfOwner')
+        .unsigned();
+      tbl.text('OwnerFeedback');
+      tbl.text('RenterFeedback');
+    })
+    //Bridge Tables below
+    .createTable('User_Stuff', tbl => {
+      tbl.integer('user_id')
+        .notNullable()
+        .unsigned()
+        .references('Users.id')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+      tbl.integer('stuff_id')
+        .notNullable()
+        .unsigned()
+        .references('Stuff.id')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+      tbl.primary(['user_id', 'stuff_id']);
+    })
+    .createTable('User_FavoriteStuff', tbl => {
+      tbl.integer('user_id')
+        .notNullable()
+        .unsigned()
+        .references('Users.id')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+      tbl.integer('stuff_id')
+        .notNullable()
+        .unsigned()
+        .references('Stuff.id')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+      tbl.primary(['user_id', 'stuff_id']);
+    })
+    .createTable('Rental_Stuff', tbl => {
+      tbl.integer('rental_id')
+        .notNullable()
+        .unsigned()
+        .references('Rentals.id')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+      tbl.integer('stuff_id')
+        .notNullable()
+        .unsigned()
+        .references('Stuff.id')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+      tbl.primary(['rental_id', 'stuff_id']);
+    })
+    .createTable('User_Rental_Owners', tbl => {
+      tbl.integer('rental_id')
+        .notNullable()
+        .unsigned()
+        .references('Rentals.id')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+      tbl.integer('owner_id')
+        .notNullable()
+        .unsigned()
+        .references('Users.id')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+      tbl.primary(['rental_id', 'owner_id']);
+    })
+    .createTable('User_Rental_Renters', tbl => {
+      tbl.integer('rental_id')
+        .notNullable()
+        .unsigned()
+        .references('Rentals.id')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+      tbl.integer('renter_id')
+        .notNullable()
+        .unsigned()
+        .references('Users.id')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+      tbl.primary(['rental_id', 'renter_id']);
+    })
     ;
 };
 
 exports.down = function(knex) {
   return knex.schema
-    .dropTableIfExists('Reviews')
     .dropTableIfExists('User_Rental_Renters')
     .dropTableIfExists('User_Rental_Owners')
     .dropTableIfExists('Rental_Stuff')
-    .dropTableIfExists('Rentals')
     .dropTableIfExists('User_FavoriteStuff')
     .dropTableIfExists('User_Stuff')
+    .dropTableIfExists('Reviews')
+    .dropTableIfExists('Rentals')
     .dropTableIfExists('Conditions')
     .dropTableIfExists('Categories')
     .dropTableIfExists('Stuff')
