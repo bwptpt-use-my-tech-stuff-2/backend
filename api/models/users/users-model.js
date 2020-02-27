13666be72b992c81b9dc813b96c6891c877fbb85
Paul M Edwards <pauledwards@gmail.com>
2020-02-27 05:28:46 -0800

    Populate users-model.js.

---
+++ api/models/users/users-model.js
@@ -0,0 +1,62 @@
+const db = require('../../../data/dbConfig.js');
+
+module.exports = {
+  createUser,
+  readUsers,
+  readUserById,
+  readUserByName,
+  updateUser,
+  deleteUser,
+};
+
+function createUser(user) {
+  if (user) {
+    return db("users")
+      .insert(user)
+      .then(u => this.readUserById(u[0]));
+  } else {
+    return null;
+  };
+};
+
+function readUsers() {
+  return db("users");
+};
+function readUserById(id) {
+  if (id) {
+    return db("users")
+      .where("id", id)
+      .first();
+  } else {
+    return null;
+  };
+};
+function readUserByName(username) {
+  if (username) {
+    return db("users")
+      .where("Email", username)
+      .first();
+  } else {
+    return null;
+  };
+};
+
+function updateUser(id, userUpdate) {
+  if (id && userUpdate) {
+    return db("users")
+      .update(userUpdate)
+      .then(count => (count > 0 ? this.readUserById(id) : null));
+  } else {
+    return null;
+  };
+};
+
+function deleteUser(id) {
+  if (id) {
+    return db("users")
+      .where("id", id)
+      .del();
+  } else {
+    return null;
+  };
+};
